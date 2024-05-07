9cb1a5bf6c04074c27a2ab5c6c20dd990f4b8b1f
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2024-05-07 11:00:35 +0530

    Update Panorama-ImageStich.cpp

--- Panorama-ImageStich.cpp
+++ Panorama-ImageStich.cpp
@@ -1,3 +1,13 @@
+/*****************************************************************************************************************************
+This C++ code uses the OpenCV library for image processing and stitching. 
+- The `load_images` function loads all image files (with .jpg or .png extension) from the specified folder path. 
+- The `resize_images` function resizes the loaded images to the specified width and height. 
+- The `stitch_images` function uses the OpenCV Stitcher class to stitch the resized images together.
+- The `crop_image` function crops the stitched image by finding the largest contour and extracting the bounding rectangle.
+- The `preview_and_save_image` function displays the cropped image and saves it to the specified folder with the folder name appended to the filename. 
+- The `panorama_stitch_folder` function orchestrates the entire process by calling the other functions in the correct order.
+*******************************************************************************************************************************/
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/stitching.hpp>
 #include <iostream>
